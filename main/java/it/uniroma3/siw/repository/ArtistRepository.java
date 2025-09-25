8c8b757e5e649ca2f9e1d2b854ca49bc12e43927
Valerio Massimini <92867591+Massi99RM@users.noreply.github.com>
2025-09-25 19:14:25 +0200

    Delete main directory

--- main/java/it/uniroma3/siw/repository/ArtistRepository.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma3.siw.repository;
-
-import it.uniroma3.siw.model.Artist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtistRepository extends JpaRepository<Artist, Long> {
-    
-}
